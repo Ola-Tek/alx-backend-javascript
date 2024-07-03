494e3a29bdcd63706a9dcb9a3ec6864aed125e5f
Obisesan Olabode <obisesanolabode10@gmail.com>
2024-07-03 04:27:58 +0000

    A module on data manipulation

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,12 @@
+/**
+ * Retrieves lists of students
+ * @Author - Ola-Tek
+ * @returns {{id, students and several other parameters
+ */
+export default function getListStudents() {
+  return [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ];
+}
---
+++ 0x03-ES6_data_manipulation/README.md
@@ -0,0 +1,2 @@
+Data Manipulations:
+working with Arrays
