45ded7f50acd8c3dba6457a14113e3e44ffb9a02
Obisesan Olabode <obisesanolabode10@gmail.com>
2024-08-20 13:53:19 +0000

    node basics

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author Obisesan Olabode <https://github.com/B3zaleel>
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
